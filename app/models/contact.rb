fc47040a3f75406e3538067e522f675736ea0ef6
Ricardo <ricardodelatorreg@gmail.com>
2015-10-03 19:00:55 +0000

    aplicando estilos al formulario de contactenos

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :message, presence: true  
-  validates :body, presence: true      
+  validates :name, length: { minimum: 7, message: "La longitud no puede ser menor a 7 caracteres" }, format: { with: /\A[a-zA-Z]+\z/, message: "Solo se permiten letras" }
+  validates :email, length: { minimum: 7, message: "La longitud no puede ser menor a 7 caracteres" }
+  validates :message,  length: { minimum: 10, message: "La longitud no puede ser menor a 10 caracteres"  }  
+  validates :body, length: { minimum: 10, message: "La longitud no puede ser menor a 10 caracteres"  }      
 end
